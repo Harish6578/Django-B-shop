e852ca323c530ae32e214ddd5373d0f19a74d8da
Harish6578 <harishvarma6578@gmail.com>
2026-01-26 19:57:31 +0530

    Upadate files

--- products/models.py
+++ products/models.py
@@ -9,3 +9,17 @@ class Product(models.Model):
     thumbnail = models.ImageField(upload_to='products/thumbnails/')
     created_at = models.DateTimeField(auto_now_add =True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class ProductImage(models.Model):
+    img = models.ImageField(upload_to='products/image/')
+    caption = models.CharField(max_length= 200, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE,related_name='images')
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.product.title} image"
